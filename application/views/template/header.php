6c27fc4efc597ed1fd640cb352853985bd99b633
bhgx <906899720@qq.com>
2016-03-22 01:02:37 +0800

    ui

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -7,11 +7,11 @@
 				// echo $_SERVER['PHP_SELF']
 				$u = $this->uri->segment(1);
 			 ?>
-			<li><a class="<?php add_current($u, 'sale') ?>" href="<?php echo site_url('/sale/lists') ?>">交易记录</a></li>
-			<li><a class="<?php add_current($u, 'customer') ?>" href="<?php echo site_url('/customer/lists') ?>">我的客户</a></li>
-			<li><a class="<?php add_current($u, 'join') ?>" href="<?php echo site_url('/join/lists') ?>">我的公司</a></li>
-			<li><a class="<?php add_current($u, 'user') ?>" href="<?php echo site_url('/user/info') ?>">账号信息</a></li>
-			<li><a class="<?php add_current($u, 'user') ?>" href="<?php echo site_url('/user/lists') ?>">用户管理</a></li>
+			<li><a class="<?php add_current($u, 'sale') ?>" href="<?php echo site_url('/sale') ?>">交易记录</a></li>
+			<li><a class="<?php add_current($u, 'customer') ?>" href="<?php echo site_url('/customer') ?>">我的客户</a></li>
+			<li><a class="<?php add_current($u, 'join') ?>" href="<?php echo site_url('/join') ?>">我的公司</a></li>
+			<li><a class="<?php add_current($u, 'user') ?>" href="<?php echo site_url('/user') ?>">账号信息</a></li>
+			<li><a class="<?php add_current($u, 'user') ?>" href="<?php echo site_url('/user') ?>">用户管理</a></li>
 		</ul>
 	</div>
 	<a class="logout" href="<?php echo site_url('/user/logout') ?>">退出</a>
